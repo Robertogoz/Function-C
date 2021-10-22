4f906ffc2a7af32fb88a742ba6b25140ee0000b5
Roberto Goz <63943375+Robertogoz@users.noreply.github.com>
2021-10-22 16:54:31 -0300

    Create ex08-funcoes.c

---
+++ ex08-funcoes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int alteracoes = 0, i, j;
+
+void printMatriz(int l, int c, int matriz[l][c]) {
+	for(i=0;i<l;i++) {
+		for(j=0;j<c;j++) {
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int processaMatriz(int l, int c, int x, int matriz[l][c]) {
+	for(i=0;i<l;i++) {
+		for(j=0;j<c;j++) {
+			if(x < matriz[i][j]) {
+				matriz[i][j] = 0;
+				alteracoes++;
+			}
+		}
+	}
+	return alteracoes;
+}
+
+int main(void) {
+	int x, alteracoes=0;
+	int matriz[3][3];
+	scanf("%d",&x);
+	for (i=0; i<3; i++){
+		for (j=0; j<3; j++){
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	alteracoes = processaMatriz(3, 3, x, matriz);
+	printMatriz(3, 3, matriz);
+	printf("Alteracoes=%d",alteracoes);
+}
